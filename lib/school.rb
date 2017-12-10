1a84c4825543acf99ad22be005aa380b3d8c0153
James Ferguson <ncjamesferguson@gmail.com>
2017-12-10 21:12:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+
+  @roster = {}
+
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_student(student_name, grade)
+
+    if !(@roster.has_key?(grade)
+      @roster[:grade] = []
+    end
+
+    @roster[:grade] << student_name
+
+end
